56805f3c8f1ab80149973577df7fc1fe7c1e9616
1oogabooga1 <dmitrijdobrydin@gmail.com>
2023-08-27 01:15:58 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmitrii Dobrydin");
+        System.out.println("10.11.2005");
+    }
+}
