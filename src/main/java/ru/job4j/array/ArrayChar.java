dd0510406d2cb8a4dabf2023db351d50549e0ec1
1oogabooga1 <dmitrijdobrydin@gmail.com>
2023-10-06 13:41:40 +0300

    6.4.0. Слово начинается с ... [#41585 #450350]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,11 +4,11 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean rsl = true;
         for (int i = 0; i < pref.length; i++) {
-                if (word[i] != pref[i]) {
-                    rsl = false;
-                    break;
+            if (word[i] != pref[i]) {
+                rsl = false;
+                break;
             }
-            }
+        }
         return rsl;
     }
 }
